cdc2c46aff6b666500aa0a10a75f980dd0bcd5eb
nuvem565 <tom.jedrzejewski565@gmail.com>
2020-12-19 16:09:34 +0100

    Constructor  of the Array class
    Sefines the .__init__(self) function for instatiation the main matrix and its methods. Generates the list of lists and adds two new tiles to it.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+class Array:
+    def __init__(self):
+        self.are_you_winning_son = False
+        self.matrix = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        # chooses two random blank fields and sets them to a value of 2
+        self.__generate_new_tile()
+        self.__generate_new_tile()
+
